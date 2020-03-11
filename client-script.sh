d0ff13e27927b0e7ec060889777c2caf0b63eaab
sabit123456 <sabit.mubarik@fiveriverstech.com.com>
2020-03-11 16:55:11 +0500

    Hoping for the best

--- client-script.sh
+++ client-script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker login -usabit -psabit1234
+docker image rmi -f $(docker images -aq)
 docker pull sabit12/my-nginx:latest
 docker run -d -p 80:80 --name my-ngninx-container sabit12/my-nginx:latest
